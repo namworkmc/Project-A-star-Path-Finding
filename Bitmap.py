d722df52580677402ba334e7bbf1f0fe9ab67194
unknown <namworkmc@gmail.com>
2021-07-09 16:26:27 +0700

    update

--- Bitmap.py
+++ Bitmap.py
@@ -28,6 +28,7 @@ class Bitmap:
         row = len(bitmapArray)
         column = len(bitmapArray[0])
         arr = np.zeros((row, column))
+        
         for i in range(len(bitmapArray)):
             for j in range (len(bitmapArray[0])):
                 arr[i][j] = int(bitmapArray[i][j][0])
